c2491d46d097d7a863ea4b2889b97be5a88597c3
Saad24 <saad.24.ahm@gmail.com>
2023-02-08 15:30:39 +0000

    copied code to user rep

--- src/main/java/com/example/Orbit/repositories/NeedRepository.java
+++ src/main/java/com/example/Orbit/repositories/NeedRepository.java
@@ -1,4 +1,5 @@
 package com.example.Orbit.repositories;
 
 public interface NeedRepository {
+
 }
--- src/main/java/com/example/Orbit/repositories/UserRepository.java
+++ src/main/java/com/example/Orbit/repositories/UserRepository.java
@@ -1,4 +1,19 @@
 package com.example.Orbit.repositories;
+import com.example.Orbit.models.Caree;
+import com.example.Orbit.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
-public interface UserRepository {
+@Repository
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    List<User> findUserById(Long id);
+
+    List<User> findUserByLocation(String location);
+
+    List<User> findUserByName(String name);
+
+    List<User> findUserByCaree(List<Caree> careeList);
 }
