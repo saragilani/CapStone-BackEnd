87004d02f54d93f89e04ab9441bd866eafe82636
saragilani <s.g.ahmed163@gmail.com>
2023-02-08 15:29:31 +0000

    added CRUD to caree controller

--- src/main/java/com/example/Orbit/controllers/CareeController.java
+++ src/main/java/com/example/Orbit/controllers/CareeController.java
@@ -1,11 +1,14 @@
 package com.example.Orbit.controllers;
 
+import com.example.Orbit.models.Caree;
 import com.example.Orbit.services.CareeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value= "/carees")
@@ -15,6 +18,29 @@ public class CareeController {
     CareeService careeService;
 
     @GetMapping
-    public ResponseEntity
+    public ResponseEntity<Optional<Caree>> getCareeById(@PathVariable Long id){
+        return new ResponseEntity<>(careeService.findCareeById(id), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Caree>> getCareeByUniqueCode(@PathVariable String uniqueCode){
+        return new ResponseEntity<>(careeService.findCareeByUniqueCode(uniqueCode), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Caree> createCaree(@RequestBody Caree caree) {
+        return new ResponseEntity<>(careeService.createCaree(caree), HttpStatus.OK);
+    }
+
+    @PatchMapping (value = "/{id}")
+    public ResponseEntity<Caree> updateCaree(@RequestBody Caree caree, @PathVariable Long id){
+        return new ResponseEntity<>(careeService.updateCaree(id, caree), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping (value = "/{id}")
+    public ResponseEntity deleteCaree(@PathVariable Long id) {
+        careeService.deleteCaree(id);
+        return new ResponseEntity("You successfully deleted the caree", HttpStatus.OK);
+    }
 
 }
