23b95f63a5537e3d7700663b2c914726401a9b88
Berna Yasar <bernayasar@hotmail.co.uk>
2023-02-08 13:42:06 +0000

    small additions to user model

--- src/main/java/com/example/Orbit/models/User.java
+++ src/main/java/com/example/Orbit/models/User.java
@@ -1,4 +1,17 @@
 package com.example.Orbit.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name="users")
+
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = Genera)
+    @Column
+
 }
