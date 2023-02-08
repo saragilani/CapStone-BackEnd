9f46842892dc1cf2251a24a5ebdf79486792229e
saragilani <s.g.ahmed163@gmail.com>
2023-02-08 14:49:43 +0000

    created caree service

--- src/main/java/com/example/Orbit/controllers/CareeController.java
+++ src/main/java/com/example/Orbit/controllers/CareeController.java
@@ -1,4 +1,20 @@
 package com.example.Orbit.controllers;
 
+import com.example.Orbit.services.CareeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value= "/carees")
 public class CareeController {
+
+    @Autowired
+    CareeService careeService;
+
+    @GetMapping
+    public ResponseEntity
+
 }
--- src/main/java/com/example/Orbit/services/CareeService.java
+++ src/main/java/com/example/Orbit/services/CareeService.java
@@ -1,4 +1,35 @@
 package com.example.Orbit.services;
 
+import com.example.Orbit.models.Caree;
+import com.example.Orbit.repositories.CareeRepository;
+import com.example.Orbit.repositories.NeedRepository;
+import com.example.Orbit.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CareeService {
+
+    @Autowired
+    NeedRepository needRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    CareeRepository careeRepository;
+
+
+    // UpdateCaree, deleteCaree, createCaree, findCareeById(), findCareeByUniqueCode()
+    public Optional<Caree> findCareeById(Long id) {
+        return careeRepository.findById(id);
+    }
+
+    public Optional<Caree> findCareeByUniqueCode(Caree caree) {
+        return careeRepository.g
+    }
 }
