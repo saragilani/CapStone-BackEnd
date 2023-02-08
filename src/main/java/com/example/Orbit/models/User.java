0cd1e0996b02c9b2992983eead22d5818c0f0676
Saad24 <saad.24.ahm@gmail.com>
2023-02-08 14:00:56 +0000

    Added user model code

--- src/main/java/com/example/Orbit/models/User.java
+++ src/main/java/com/example/Orbit/models/User.java
@@ -1,9 +1,10 @@
 package com.example.Orbit.models;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity(name="users")
 
@@ -11,7 +12,81 @@ import javax.persistence.Id;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = Genera)
-    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private Long id;
 
+    @Column(name="name")
+    private String name;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="emailAddress")
+    private String emailAddress;
+
+    @Column(name="location")
+    private String location;
+
+    @JsonIgnoreProperties("users")
+    @OneToMany(mappedBy = "users")
+    private List<Need> toDoList;
+
+    //
+    public User(String name, String password, String emailAddress, String location){
+        this.name = name;
+        this.password = password;
+        this.emailAddress = emailAddress;
+        this.location = location;
+        this.toDoList = new ArrayList<>();
+    }
+    public User(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Need> getToDoList() {
+        return toDoList;
+    }
+
+    public void setToDoList(List<Need> toDoList) {
+        this.toDoList = toDoList;
+    }
 }
